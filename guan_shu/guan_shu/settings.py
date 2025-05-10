f0aa8ce64842381a1082ec76f2fc4a942a6132ca
t3m14 <artemon.m174@gmail.com>
2025-05-10 22:55:07 +0300

    settings changed

--- guan_shu/guan_shu/settings.py
+++ guan_shu/guan_shu/settings.py
@@ -61,12 +61,14 @@ LOGOUT_REDIRECT_URL = 'home'
 # Статические файлы
 STATIC_URL = "/static"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
